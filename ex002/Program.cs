8078cf6b3578fb08ea2bb0d1bdfa8e29d3d4c093
«Elena <lingelena@hotmail.com>
2022-04-26 16:21:48 +0200

    Added ex002 Hello user

--- ex002/Program.cs
+++ ex002/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, nice World!");
+﻿// Greetings while entering an username
+Console.Write("Enter your name, please: ");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username!);
