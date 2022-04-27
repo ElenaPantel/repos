49a15f6759f297425d6b94d1f140350af4247e15
«Elena <lingelena@hotmail.com>
2022-04-27 12:00:00 +0200

    Added ex 005 Condition 99 if-else

---
+++ ex005Condition99IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ex005 Condition 99 if else
+Console.Write("Your name, please: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+Console.WriteLine("Super, this is MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}   
