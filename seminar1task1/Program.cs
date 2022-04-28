b62d886d3c57956c58a864b2596fbe86bfc7c7a3
«Elena <lingelena@hotmail.com>
2022-04-28 22:51:28 +0200

    Seminar1 task1 How to square numbers Secondvariant with Parse

---
+++ seminar1task1/Program.cs
@@ -0,0 +1,7 @@
+﻿// 2ой вариант программы, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя)
+
+int number = int.Parse(Console.ReadLine());
+
+int numberResult = number * number;
+
+Console.WriteLine(numberResult);
