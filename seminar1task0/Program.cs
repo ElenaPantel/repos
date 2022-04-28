49e21713210cfed26c44def84271286e8b35ba1e
«Elena <lingelena@hotmail.com>
2022-04-28 19:40:12 +0200

    Seminar1 task0 added

---
+++ seminar1task0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя)
+
+int number = 5;
+
+//int numberResult = number * number;
+
+double numberResult = Math.Pow(number, 3);
+
+Console.WriteLine(numberResult);
