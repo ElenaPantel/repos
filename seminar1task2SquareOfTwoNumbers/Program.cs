5c2b3d8650b74c494462958be4c2ddd459ad86a2
«Elena <lingelena@hotmail.com>
2022-04-29 12:17:55 +0200

    Seminar1 task2 bool yes no Square of two numbers

---
+++ seminar1task2SquareOfTwoNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и 
+// проверяет, является ли первое число квадратом второго
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+bool number1square = number1 * number1 == number2;
+
+Console.WriteLine(number1square ? "Yes" : "No");
+
+//if(number1Square == number2)
+//{
+//   Console.WriteLine("Number1 is the square of number2"); 
+//}
+//else
+//{
+//    Console.WriteLine("Number1 is not the square of number2");
+//}
