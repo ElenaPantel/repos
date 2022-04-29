9779632aa38e2c3dd073fa770f88c89c62a26986
«Elena <lingelena@hotmail.com>
2022-04-29 21:37:34 +0200

    DZ_1_4_Seminar1 for Interval even numbers

---
+++ DZ_1_4_Seminar1_Interval_even_numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая на вход принимает число (N),
+// на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Enter a number, please => ");
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 2; i <= n; i=i+2)
+{
+    Console.Write(i + " ");
+}
