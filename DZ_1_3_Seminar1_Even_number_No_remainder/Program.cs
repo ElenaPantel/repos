fd2f89db6381b57d2d7392b4339f9675146ccb27
«Elena <lingelena@hotmail.com>
2022-04-29 18:49:09 +0200

    DZ_1_3_Seminar1 bool Even number without remainder or an odd number

---
+++ DZ_1_3_Seminar1_Even_number_No_remainder/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Enter any number, please: ");
+
+int number = int.Parse(Console.ReadLine());
+
+bool numbereven = number % 2 == 0;
+
+Console.WriteLine(numbereven ? "Yes, it's even" : "No, it's odd");
