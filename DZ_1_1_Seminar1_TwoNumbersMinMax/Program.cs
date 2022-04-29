6676732366149f892a3786d1e3f0ee13175f70ad
«Elena <lingelena@hotmail.com>
2022-04-29 17:16:32 +0200

    DZ_1_1_Seminar1 Two Numbers Max and Min

---
+++ DZ_1_1_Seminar1_TwoNumbersMinMax/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая на вход принимает два числа и 
+// выдаёт, какое число большее, а какое меньшее
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+int max = number1;
+int min = number2;
+
+if (number2 > max) max = number2;
+if (number1 < number2) min = number1;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
