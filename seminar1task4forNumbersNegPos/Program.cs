b088ca5b2cce4eea35707ae7567993c460cf0337
«Elena <lingelena@hotmail.com>
2022-04-29 15:24:12 +0200

    Seminar1 task 4 Shows all integer numbers within an interval from its negative and its positive

---
+++ seminar1task4forNumbersNegPos/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая на вход принимает одно число (N), 
+// на выходе показывает все целые числа в промежутке от -N до N
+
+Console.Write("Enter an integer number, please => ");
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i = -n; i <= n; i++)
+{
+    Console.Write(i + " ");
+}
