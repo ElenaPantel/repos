1e570345a7e3f04deeabd5527f0b611cc028dfb1
«Elena <lingelena@hotmail.com>
2022-04-29 12:49:55 +0200

    Seminar1 task3 Number of each day of the week

---
+++ seminar1task3numberWeekdays/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру
+
+string numberweek = Console.ReadLine();
+if (numberweek == "1") Console.WriteLine("Monday");
+if (numberweek == "2") Console.WriteLine("Tuesday");
+if (numberweek == "3") Console.WriteLine("Wednesday");
+if (numberweek == "4") Console.WriteLine("Thursday");
+if (numberweek == "5") Console.WriteLine("Friday");
+if (numberweek == "6") Console.WriteLine("Saturday");
+if (numberweek == "7") Console.WriteLine("Sunday");
