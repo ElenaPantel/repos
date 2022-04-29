80484075e0c6305bba1bc9e8fd28d665b01693bd
«Elena <lingelena@hotmail.com>
2022-04-29 23:06:13 +0200

    DZ_1_5_Seminar1 Random 3 digit numbers with the last digit output

---
+++ DZ_1_5_Seminar1_Random3digitNumbers/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// на выходе показывает последнюю цифру этого числа.
+
+int inputNumber = new Random().Next(100, 1000);
+int outputNumber = inputNumber % 10;
+
+Console.WriteLine("Random number: " + inputNumber);
+Console.WriteLine("Last digit of the number: " + outputNumber);
